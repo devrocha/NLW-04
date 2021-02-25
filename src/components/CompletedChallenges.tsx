3815740ed4a5e09d53590663f6780d1f7fb55d7e
Matheus Rocha <mrochasandim75@gmail.com>
2021-02-25 15:03:08 -0400

    feat: contexts final aula 03

--- src/components/CompletedChallenges.tsx
+++ src/components/CompletedChallenges.tsx
@@ -1,13 +1,18 @@
-import styles from '../styles/components/CompletedChallanges.module.css'
+import { useContext } from 'react'
+import { ChallengesContext } from '../contexts/ChallengeContext'
+import styles from '../styles/components/CompletedChallenges.module.css'
 
 export function CompletedChallenges () {
+
+    const { challengesCompleted } = useContext(ChallengesContext)
+
     return (
         <div className={styles.completedChalangesContainer}>
             <span>
                 Desafios completos
             </span>
 
-            <span> 5 </span>
+            <span> {challengesCompleted} </span>
         </div>
     )
-}
+}   
--- src/components/Countdown.tsx
+++ src/components/Countdown.tsx
@@ -1,10 +1,15 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useContext } from 'react'
+import { ChallengesContext } from '../contexts/ChallengeContext';
+
 import styles from '../styles/components/Countdown.module.css'
 
 let countdownTimeout: NodeJS.Timeout;
 
 export function Countdown() {
-    const [time, setTime] = useState(25 * 60);
+
+    const { startNewChallenge, resetChallenge } = useContext(ChallengesContext)
+
+    const [time, setTime] = useState(0.05 * 60);
     const [isActive, setIsActive] = useState(false)
     const [hasFinished, setHasFinished] = useState(false)
 
@@ -21,7 +26,7 @@ export function Countdown() {
     function resetCountdown() {
         clearTimeout(countdownTimeout);
         setIsActive(false);
-        setTime(25 * 60)
+        setTime(0.05 * 60)
     }
 
     useEffect(() => {
@@ -32,6 +37,7 @@ export function Countdown() {
         } else if (isActive && time === 0) {
             setHasFinished(true);
             setIsActive(false);
+            startNewChallenge();
         }
     }, [isActive, time])
 
